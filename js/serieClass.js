9a743094478f102f7970f7e2c27000efa5b7ce7a
Marcelo <marcelo.s44@gmail.com>
2023-04-18 20:29:33 -0500

    Add class series

---
+++ js/serieClass.js
@@ -0,0 +1,10 @@
+export class NewSerie {
+    constructor(code, title, description, image, genre) {
+        this.code = code;
+        this.title = title;
+        this.description = description;
+        this.image = image;
+        this.genre = genre;
+    }
+    //agregar metodos necesarios
+}
