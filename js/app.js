e34a0b9e7cdf8d6ea424ab3eec0a8292da8e1f74
Marcelo <marcelo.s44@gmail.com>
2023-07-10 22:31:09 -0500

    Enviamos parametro por URL

--- js/app.js
+++ js/app.js
@@ -4,21 +4,29 @@ console.log(listarSeries);
 //Si hay datos dibujar kas tarjetas
 
 listarSeries.forEach(serie => {
-    createColumn(serie);
+  createColumn(serie);
 });
 
 function createColumn(serie) {
-    let gridSeries = document.querySelector('#grid-series');
-    gridSeries.innerHTML += `
-        <article class="col-12 col-md-4 col-lg-3 mb-3">
+  let gridSeries = document.querySelector('#grid-series');
+  gridSeries.innerHTML += `
+          <article class="col-12 col-md-4 col-lg-3 mb-3">
             <div class="card">
               <img class="img-top__card-container" src="${serie.image}" alt="${serie.title}">
               <div class="card-body">
                 <h5 class="card-title">${serie.title}</h5>
-                <a href="#" class="btn btn-primary">Ver detalles</a>
+                <button href="#" class="btn btn-primary" onclick='verDetalle(${serie.code})'>Ver detalles</button>
               </div>
             </div>
           </article>
-  
-  `
+          `
+};
+
+//Navegavcion
+window.verDetalle = (code) => {
+  console.log(code);
+  console.log(window.location);
+  console.log(window.location.origin + `/pages/detail.html?code=${code}`);
+  window.location.href = window.location.origin + `/pages/detail.html?code=${code}`;
 }
+
